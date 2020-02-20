895b2f4f16448d1b31f0eae130ae818eb62376c5
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-02-20 15:38:45 -0300

    mini curso ReactJS concluido

---
+++ rocketseat-reactjs/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './pages/main';
+import Routes from './routes';
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
