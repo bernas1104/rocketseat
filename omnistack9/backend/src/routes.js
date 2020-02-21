502a6b25e8edefecd55d64031836104a17e3e9a9
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-02-21 16:44:20 -0300

    backend done

--- omnistack9/backend/src/routes.js
+++ omnistack9/backend/src/routes.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const SessionsController = require('./controllers/SessionsController');
+const SpotsController = require('./controllers/SpotsController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingsController = require('./controllers/BookingsController');
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-// req.query => acessar os parâmetros da query
-// req.params => acessar os parâmetros da rota
-// req.body => acessar corpo da requisição
+routes.post('/sessions', SessionsController.store);
 
-routes.get('/', (req, res) => {
-    return res.json({
-        "hello": "Hello, World!!",
-    });
-});
+routes.get('/spots', SpotsController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotsController.store);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingsController.store);
 
 module.exports = routes;
--- omnistack9/backend/src/server.js
+++ omnistack9/backend/src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://bernas1104:110492-Bb@omnistack-9sqiq.mongodb.net/test?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);
 
 app.listen(3000);   // Utiliza a porta 3000
+
+// req.query ==> Acessar query params (para filtros)
+// req.params ==> Acessar route params (para edição/delete)
+// req.body ==> Acessar corpo da requisição (para criação/edição)
